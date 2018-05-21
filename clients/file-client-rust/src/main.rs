d8356ebc3b4ecff57b7e5b36a4c267e494590106
Ryan Plauche <ryan@kubos.co>
2018-05-21 16:46:03 -0500

    Work on client to use cbor-protocol in sending.

--- clients/file-client-rust/src/main.rs
+++ clients/file-client-rust/src/main.rs
@@ -1,17 +1,18 @@
 #[macro_use]
 extern crate log;
+extern crate file_protocol;
 extern crate simplelog;
 
-mod lib;
-
-use lib::file_protocol as protocol;
 use simplelog::*;
 
-fn upload(source_path: &str, target_path: &str) {
-    info!("Uploading local:{} to remote:{}", source_path, target_path);
-    let (hash, num_chunks, mode) = protocol::local_import(source_path).unwrap();
-    protocol::send_sync(&hash, num_chunks);
-}
+use file_protocol::CborProtocol;
+use file_protocol::FileProtocol;
+
+// fn upload(source_path: &str, target_path: &str) {
+//     info!("Uploading local:{} to remote:{}", source_path, target_path);
+//     let (hash, num_chunks, mode) = protocol::local_import(source_path).unwrap();
+//     protocol::send_sync(&hash, num_chunks);
+// }
 
 fn main() {
     CombinedLogger::init(vec![
@@ -20,8 +21,21 @@ fn main() {
 
     info!("Starting communications service");
 
-    let source = String::from("/home/ryan/Pictures/photoexamples.png");
-    let dest = String::from("/home/ryan/test2.txt");
+    let source_path = String::from("/home/ryan/Pictures/photoexamples.png");
+    let target_path = String::from("/home/ryan/test2.txt");
 
-    upload(&source, &dest);
+    let c_protocol = CborProtocol::new(0);
+
+    let f_protocol = FileProtocol {
+        cbor_proto: c_protocol,
+        host: String::from("127.0.0.1"),
+        dest_port: 7000,
+    };
+    info!(
+        "Uploading local:{} to remote:{}",
+        &source_path, &target_path
+    );
+    let (hash, num_chunks, mode) = f_protocol.local_import(&source_path).unwrap();
+    f_protocol.send_sync(&hash, num_chunks).unwrap();
+    f_protocol.send_export(&hash, &target_path, mode);
 }
